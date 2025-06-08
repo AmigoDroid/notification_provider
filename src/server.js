fdf5092295e7e0fd6693d1e106190af0ea98b2c7
Luciano <canalmeuovo36@gmail.com>
2025-06-08 13:46:21 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -7,10 +7,13 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({origin: '*'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on`);
 });
