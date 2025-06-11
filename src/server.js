e200c09afd674a8eb9549bc4502d9b1da054bb92
Luciano <canalmeuovo36@gmail.com>
2025-06-11 13:36:48 -0300

    esqueci de abrir a rota, abrindo a rota

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
     message: 'Welcome to the API!'
   });
 });
-app.get("getNotify", (req, res) => {
+app.get("/getNotify", (req, res) => {
   res.json({
     message: 'endpoint getNotify is working!',
     seila:"depois vou ver uma forma melhor de fazer isso"
