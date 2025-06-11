04010f99c915b81383108da1afd8d436a9b990ad
Luciano <canalmeuovo36@gmail.com>
2025-06-11 13:33:27 -0300

    add route getnotify

--- src/server.js
+++ src/server.js
@@ -15,6 +15,12 @@ app.get('/', (req, res) => {
     message: 'Welcome to the API!'
   });
 });
+app.get("getNotify", (req, res) => {
+  res.json({
+    message: 'endpoint getNotify is working!',
+    seila:"depois vou ver uma forma melhor de fazer isso"
+  });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on`);
