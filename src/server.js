5f76c637468fcee88f20e63246b714f88e17ed4e
Luciano <canalmeuovo36@gmail.com>
2025-06-24 13:46:31 -0300

    teste de rota

--- src/server.js
+++ src/server.js
@@ -1,27 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import router from './routes/router.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors({origin: '*'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
+app.use("/api",router);
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to the API!'
-  });
-});
-app.get("/getNotify", (req, res) => {
-  res.json({
-    message: 'endpoint getNotify is working!',
-    seila:"depois vou ver uma forma melhor de fazer isso"
+    IP: req.ip.replace('::ffff:', ''),
+    ISP: req.headers['x-forwarded-for'] || req.connection.remoteAddress
   });
 });
 
+
 app.listen(PORT, () => {
-    console.log(`Server is running on`);
+    console.log(`Server is running on port ${PORT}`);
 });
